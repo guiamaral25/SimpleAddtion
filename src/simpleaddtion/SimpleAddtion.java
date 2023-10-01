dd951fbdab31ddef64915ee71305e7f787a8815a
guiamaral25 <guiamaral25@gmail.com>
2023-10-01 20:58:22 +0100

    Initial commit

---
+++ src/simpleaddtion/SimpleAddtion.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package simpleaddtion;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author guiam
+ */
+public class SimpleAddtion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+  
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
+
+        
+   
+    
+
